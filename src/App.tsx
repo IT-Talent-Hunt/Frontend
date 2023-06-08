0a8c01c785fc813796d5bdefa9b8f02a63cc5267
Artem Rymarchuk <rumarchyk204@gmail.com>
2023-06-08 13:57:05 +0200

    First steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import './App.scss';
+import { Header } from './components/Header/Header';
 
 interface Props {
   onClick: () => void;
 }
 
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
+  <button type="button" onClick={onClick}>
+    {children}
+  </button>
+));
 
 export const App: React.FC = () => {
   return (
     <div className="starter">
       <Provider onClick={() => ({})}>
-        <TodoList />
+        <Header></Header>
       </Provider>
     </div>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+export const Header: FC = () => {
+  return (
+    <header>
+      This is test Header
+    </header>
+  );
+};
