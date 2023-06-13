2626428394fdea8e8a4a0a6446fe9dc2c3f7d6c3
Artem Rymarchuk <rumarchyk204@gmail.com>
2023-06-13 13:49:21 +0200

    SignUp form and validation not tested

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,18 +1,45 @@
 import { FC, FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './LoginForm.module.scss';
+import { FormState } from '../../Types/FormState';
+import { validation } from '../../helpers/validation';
 
 export const LoginForm: FC = () => {
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [confirmPassword, setConfirmPassword] = useState('');
+  // const [email, setEmail] = useState('');
+  const [state, setState] = useState<FormState>({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [errors, setErrors] = useState<FormState>({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [isSigningUp, setIsSigningUp] = useState(false);
   const navigate = useNavigate();
+
+  const handleInput = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setState({ ...state, [evt.target.name]: [evt.target.value] });
+  };
+
+  const handleKeyDown = (evt: React.KeyboardEvent<HTMLSpanElement>) => {
+    if (evt.key === 'Enter') {
+      setIsSigningUp(true);
+    }
+  };
 
   const handleSubmit = (evt: FormEvent) => {
     evt.preventDefault();
 
-    localStorage.setItem('isSignedIn', 'true');
+    // localStorage.setItem('isSignedIn', 'true');
+    setErrors(validation(state));
 
-    navigate('/main');
+    if (!errors.email && !errors.password && !errors.confirmPassword) {
+      navigate('/main');
+    }
   };
 
   return (
@@ -24,34 +51,92 @@ export const LoginForm: FC = () => {
         </label>
         <input
           type="email"
-          required
+          name="email"
           id="email"
-          value={email}
-          onChange={(evt) => setEmail(evt.target.value)}
+          // required
+          value={state.email}
+          onChange={(evt) => handleInput(evt)}
           className={styles.input}
           placeholder="Enter email"
         />
+        {
+          errors.email && <span>{ errors.email }</span>
+        }
         <label htmlFor="password" className={styles.label}>
           Password
         </label>
         <input
           type="password"
-          required
-          minLength={5}
           id="password"
-          value={password}
-          onChange={(evt) => setPassword(evt.target.value)}
+          name="password"
+          // required
+          minLength={5}
+          value={state.password}
+          onChange={(evt) => handleInput(evt)}
           className={styles.input}
-          placeholder="Enter your password"
+          placeholder="Enter password"
         />
-        <button type="submit" className={styles.button}>
-          Sign in
-        </button>
+        {
+          errors.password && <span>{ errors.password }</span>
+        }
+        {isSigningUp && (
+          <>
+            <label htmlFor="confirmPassword" className={styles.label}>
+              Confirm password
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              // required
+              minLength={5}
+              value={state.confirmPassword}
+              onChange={(evt) => handleInput(evt)}
+              className={styles.input}
+              placeholder="Repeat password"
+            />
+            {
+              errors.confirmPassword && <span>{ errors.confirmPassword }</span>
+            }
+          </>
+        )}
+        {isSigningUp ? (
+          <button type="submit" className={styles.button}>
+            Sign up
+          </button>
+        ) : (
+          <button type="submit" className={styles.button}>
+            Sign in
+          </button>
+        )}
       </form>
-      <span className={styles.span}>
-        Don&apos;t have an account yet?
-        <a href="/" className={styles.link}> Register now</a>
-      </span>
+      {isSigningUp ? (
+        <span className={styles.span}>
+          Have an account?&nbsp;
+          <span
+            className={styles.link}
+            onClick={() => setIsSigningUp(false)}
+            onKeyDown={(evt) => handleKeyDown(evt)}
+            role="link"
+            tabIndex={0}
+          >
+            Sign in
+          </span>
+        </span>
+      ) : (
+        <span className={styles.span}>
+          Don&apos;t have an account yet?&nbsp;
+          <span
+            className={styles.link}
+            onClick={() => setIsSigningUp(true)}
+            onKeyDown={(evt) => handleKeyDown(evt)}
+            role="link"
+            tabIndex={0}
+          >
+            Register now
+          </span>
+        </span>
+      )}
     </div>
   );
 };
---
+++ src/helpers/validation.ts
@@ -0,0 +1,30 @@
+import { FormState } from '../Types/FormState';
+
+export const validation = (state: FormState): FormState => {
+  const errors = {
+    email: '',
+    password: '',
+    confirmPassword: '',
+  };
+
+  const email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
+
+  if (state.email === '') {
+    errors.email = 'Email should not be empty';
+  } else if (!email_pattern.test(state.email)) {
+    errors.email = 'Invalid email';
+  }
+
+  if (state.password === '') {
+    errors.password = 'Password should not be empty';
+  } else if (!password_pattern.test(state.password)) {
+    errors.password = 'Password should have at least 1 capital and small letters and a number';
+  }
+
+  if (state.confirmPassword === '' || state.confirmPassword !== state.password) {
+    errors.confirmPassword = 'Passwords aren`t matching';
+  }
+
+  return errors;
+};
