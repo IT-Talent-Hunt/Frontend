ef351e17544184844c1171be915efc186453daf6
Artem Rymarchuk <rumarchyk204@gmail.com>
2023-06-15 12:34:49 +0200

    small yet important addition

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -8,7 +8,13 @@ type Props = {
 
 export const IconButton: FC<Props> = ({ svg, submit = false }) => {
   return (
-    <button type={submit ? 'submit' : 'button'} className={style.button}>
+    <button
+      type={submit ? 'submit' : 'button'}
+      className={style.button}
+      onClick={
+        submit ? () => {} : () => window.open('https://youtu.be/dQw4w9WgXcQ')
+      }
+    >
       <div
         style={{ backgroundImage: `url(${svg})` }}
         className={style.icon}
