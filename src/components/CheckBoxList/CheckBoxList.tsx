a7f5b38f9f10f992798d6da7bb088c4d31017447
Artem Rymarchuk <rumarchyk204@gmail.com>
2023-06-16 11:47:31 +0200

    CheckBoxList component was made. all checkBox lists rendered

---
+++ src/components/CheckBoxList/CheckBoxList.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import styles from './CheckBoxList.module.scss';
+
+type Props = {
+  list: string[];
+  callbackFn: (
+    evt: React.ChangeEvent<HTMLInputElement>,
+    stateType: 'position' | 'status' | 'technologies'
+  ) => void;
+  stateType: 'position' | 'status' | 'technologies';
+  heading: string;
+  state: string[];
+};
+
+export const CheckBoxList: FC<Props> = ({
+  list, callbackFn, stateType, heading, state,
+}) => {
+  return (
+    <div className={styles.list}>
+      <h4 className={styles.heading}>{heading}</h4>
+      <ul className={styles.list__ul}>
+        {list.map((el) => (
+          <li key={el} className={styles.list__item}>
+            <input
+              type="checkbox"
+              checked={state.includes(el.toLowerCase())}
+              name={el.toLowerCase()}
+              id={el.toLowerCase()}
+              className={styles.checkbox}
+              onChange={(evt) => callbackFn(evt, stateType)}
+            />
+            <label htmlFor={el.toLowerCase()} className={styles.label}>
+              {el}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,43 +1,54 @@
-import { FC, useState } from 'react';
+import { FC, useCallback, useState } from 'react';
 import styles from './sideBar.module.scss';
+import { CheckBoxList } from '../CheckBoxList/CheckBoxList';
 
 export const SideBar: FC = () => {
   const [positions, setPositions] = useState<string[]>([]);
+  const [statuses, setStatuses] = useState<string[]>([]);
+  const [technologies, setTechnologies] = useState<string[]>([]);
   const preMadePositions = ['Front-end developer', 'Back-end developer', 'Full-stack developer', 'DevOps', 'QA', 'Project Manager', 'UI/UX Designer'];
+  const preMadeStatuses = ['All', 'Recruitment', 'In progress', 'Finished'];
+  const preMadeTechnologies = ['TypeScript', 'Java', 'C#', 'Python', 'Assembly', 'Figma', 'HTML/CSS'];
 
-  const handleCheckbox = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    const position = evt.target.name;
+  const handleCheckbox = useCallback((evt: React.ChangeEvent<HTMLInputElement>, stateType: 'position' | 'status' | 'technologies') => {
+    const val = evt.target.name;
 
-    if (positions.includes(position)) {
-      setPositions(positions.filter((el) => el !== position));
-    } else {
-      setPositions([...positions, position]);
+    if (stateType === 'position') {
+      if (positions.includes(val)) {
+        setPositions(positions.filter((el) => el !== val));
+      } else {
+        setPositions([...positions, val]);
+      }
+    } else if (stateType === 'status') {
+      if (statuses.includes(val)) {
+        setStatuses(statuses.filter((el) => el !== val));
+      } else {
+        setStatuses([...statuses, val]);
+      }
+    } else if (stateType === 'technologies') {
+      if (technologies.includes(val)) {
+        setTechnologies(technologies.filter((el) => el !== val));
+      } else {
+        setTechnologies([...technologies, val]);
+      }
     }
+  }, [statuses, positions]);
+
+  const handleClearAll = () => {
+    setPositions([]);
+    setStatuses([]);
+    setTechnologies([]);
   };
 
   return (
     <div className={styles.main}>
       <div className={styles.heading__container}>
         <h4 className={styles.heading}>Filters</h4>
-        <button type="button">Clear all</button>
+        <button type="button" onClick={handleClearAll}>Clear all</button>
       </div>
-      <div className={styles.position__container}>
-        <h4 className={styles.heading}>Position</h4>
-        <ul className={styles.position__list}>
-          {preMadePositions.map((el) => (
-            <li key={el} className={styles.position__item}>
-              <input
-                type="checkbox"
-                name={el.toLowerCase()}
-                id={el.toLowerCase()}
-                className={styles.checkbox}
-                onChange={(evt) => handleCheckbox(evt)}
-              />
-              <label htmlFor={el.toLowerCase()} className={styles.label}>{el}</label>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <CheckBoxList list={preMadePositions} callbackFn={handleCheckbox} stateType="position" heading="Position" state={positions} />
+      <CheckBoxList list={preMadeStatuses} callbackFn={handleCheckbox} stateType="status" heading="Status" state={statuses} />
+      <CheckBoxList list={preMadeTechnologies} callbackFn={handleCheckbox} stateType="technologies" heading="Technologies" state={technologies} />
     </div>
   );
 };
