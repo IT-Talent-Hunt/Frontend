d5c705393e7e6adc9faf4118eef7f2301237dacc
Artem Rymarchuk <rumarchyk204@gmail.com>
2023-06-20 13:37:36 +0200

    card component implemented with the truncateText helper function

---
+++ src/Types/ProjectCardProps.ts
@@ -0,0 +1,10 @@
+export interface ProjectCardProps {
+  title: string;
+  owner: string;
+  status: string;
+  members: number;
+  maxMembers: number;
+  description: string;
+  creationDate: string;
+  isFavorite: boolean;
+}
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import styles from './MainPage.module.scss';
 import { SideBar } from '../../components/SideBar/SideBar';
 import { GridHeader } from '../../components/GridHeader/GridHeader';
+import { ProjectCard } from '../../components/projectCard/ProjectCard';
 
 export const MainPage: FC = () => {
   return (
@@ -9,6 +10,18 @@ export const MainPage: FC = () => {
       <SideBar />
       <div className={styles.grid__container}>
         <GridHeader n={24} position="" />
+        <ProjectCard
+          title="Taskify"
+          status="In progress"
+          owner="Joe Biden"
+          members={4}
+          maxMembers={5}
+          description={
+            '"Taskify" is a simple web application for task management with collaboration. The goal is to provide organization and productivity in the work environment.'
+          }
+          creationDate="16.06.2023"
+          isFavorite
+        />
       </div>
     </div>
   );
