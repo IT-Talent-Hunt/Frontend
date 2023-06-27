4a50bf377002d1bb0b278dd951ac647a9630a04a
Artem Rymarchuk <rumarchyk204@gmail.com>
2023-06-27 11:33:04 +0200

    safety saves

--- src/components/ProjectModal/ProjectModal.tsx
+++ src/components/ProjectModal/ProjectModal.tsx
@@ -1,9 +1,15 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { FC } from 'react';
 import styles from './ProjectModal.module.scss';
 import { ProjectCardProps } from '../../Types/ProjectCardProps';
 import favorite from '../../svg/heartEmpty.svg';
 
-export const ProjectModal: FC<ProjectCardProps> = ({
+interface Props extends ProjectCardProps {
+  setShowModal: (value: React.SetStateAction<boolean>) => void;
+}
+
+export const ProjectModal: FC<Props> = ({
   title,
   owner,
   status,
@@ -12,39 +18,42 @@ export const ProjectModal: FC<ProjectCardProps> = ({
   description,
   creationDate,
   isFavorite,
+  setShowModal,
 }) => {
   return (
     <div className={styles.modal}>
-      <div className={styles.heading}>
-        <div className={styles.heading__container}>
-          <h2 className={styles.title}>{title}</h2>
-          <div className={styles.status}>{status}</div>
+      <div className={styles.background} onClick={() => setShowModal(false)}>
+        <div className={styles.heading}>
+          <div className={styles.heading__container}>
+            <h2 className={styles.title}>{title}</h2>
+            <div className={styles.status}>{status}</div>
+          </div>
+          <div
+            className={styles.favorite}
+            style={{
+              backgroundImage: isFavorite
+                ? `url(${favorite})`
+                : `url(${favorite})`,
+            }}
+          >
+          </div>
         </div>
-        <div
-          className={styles.favorite}
-          style={{
-            backgroundImage: isFavorite
-              ? `url(${favorite})`
-              : `url(${favorite})`,
-          }}
-        >
+        <div className={styles.owner}>
+          <h5 className={styles.owner__name}>{owner}</h5>
         </div>
-      </div>
-      <div className={styles.owner}>
-        <h5 className={styles.owner__name}>{owner}</h5>
-      </div>
-      <div className={styles.members}>
-        <div className={styles.members__icon} />
-        {`${members}/${maxMembers} members`}
-      </div>
-      <p className={styles.description}>{description}</p>
-      <div className={styles.footer}>
-        <button type="button" className={styles.btn}>
-          Apply
-        </button>
-        <div className={styles.date__container}>
-          <div className={styles.calendar__icon} />
-          {creationDate}
+        <div className={styles.members}>
+          <div className={styles.members__icon} />
+          {`${members}/${maxMembers} members`}
+        </div>
+        <p className={styles.description}>{description}</p>
+        <div className={styles.footer}>
+          <button type="button" className={styles.btn}>
+            Apply
+          </button>
+          <div className={styles.date__container}>
+            <div className={styles.calendar__icon} />
+            {creationDate}
+          </div>
         </div>
       </div>
     </div>
--- src/components/projectCard/ProjectCard.tsx
+++ src/components/projectCard/ProjectCard.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { FC, useState } from 'react';
+import {
+  FC, useEffect, useRef, useState,
+} from 'react';
 import { ProjectCardProps } from '../../Types/ProjectCardProps';
 import styles from './ProjectCard.module.scss';
 import favorite from '../../svg/heartEmpty.svg';
 import { truncateText } from '../../helpers/truncateText';
+import { ProjectModal } from '../ProjectModal/ProjectModal';
 
 export const ProjectCard: FC<ProjectCardProps> = (
   {
@@ -20,33 +23,51 @@ export const ProjectCard: FC<ProjectCardProps> = (
   const [showModal, setShowModal] = useState(false);
   const shortDescp = truncateText(description, 120);
 
-  const handleCardClick = (evt: React.MouseEvent<HTMLDivElement>) => {
-    const target = evt.target as HTMLDivElement;
-    const hasCardClass = target.classList.contains(styles.card);
+  const handleCardClick = () => {
+    setShowModal(true);
+  };
 
-    if (hasCardClass) {
-      setShowModal(true);
-    }
+  const handleApplyClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation(); // Prevent click event from bubbling up to the card
+  };
+
+  const handleFavoritesClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation(); // Prevent click event from bubbling up to the card
   };
 
   return (
     /* The <div> element has a child <button> element that allows keyboard interaction */
     /* eslint-disable-next-line jsx-a11y/no-static-element-interactions */
-    <div className={styles.card} onClick={handleCardClick} onKeyDown={() => {}}>
+    <div className={styles.card} onClick={handleCardClick} onKeyDown={() => { }}>
+      {showModal && (
+        <ProjectModal
+          title={title}
+          status={status}
+          owner={owner}
+          members={members}
+          maxMembers={maxMembers}
+          isFavorite={isFavorite}
+          creationDate={creationDate}
+          description={description}
+          setShowModal={setShowModal}
+        />
+      )}
       <div className={styles.heading}>
         <div className={styles.heading__container}>
           <h2 className={styles.title}>{title}</h2>
           <div className={styles.status}>{status}</div>
         </div>
-        <div
+        <button
+          type="button"
           className={styles.favorite}
           style={{
             backgroundImage: isFavorite
               ? `url(${favorite})`
               : `url(${favorite})`,
           }}
+          onClick={handleFavoritesClick}
         >
-        </div>
+        </button>
       </div>
       <div className={styles.owner}>
         <h5 className={styles.owner__name}>{owner}</h5>
@@ -55,11 +76,11 @@ export const ProjectCard: FC<ProjectCardProps> = (
         <div className={styles.members__icon} />
         {`${members}/${maxMembers} members`}
       </div>
-      <p className={styles.description}>
-        {shortDescp}
-      </p>
+      <p className={styles.description}>{shortDescp}</p>
       <div className={styles.footer}>
-        <button type="button" className={styles.btn}>Apply</button>
+        <button type="button" className={styles.btn} onClick={handleApplyClick}>
+          Apply
+        </button>
         <div className={styles.date__container}>
           <div className={styles.calendar__icon} />
           {creationDate}
